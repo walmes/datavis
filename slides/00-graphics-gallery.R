38e1752547237c059aa951e2298fa06bc0a8220c
Walmes Zeviani <walmes@ufpr.br>
2019-04-10 15:09:09 -0300

    Updates script and slides.

--- slides/00-graphics-gallery.R
+++ slides/00-graphics-gallery.R
@@ -1,8 +1,15 @@
 #---- read_futbol ------------------------------------------------------
 
+suppressPackageStartupMessages(library(lattice))
+suppressPackageStartupMessages(library(latticeExtra))
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(gridExtra))
 suppressPackageStartupMessages(library(ggjoy))
+suppressPackageStartupMessages(library(waffle))
+suppressPackageStartupMessages(library(ggmosaic))
+suppressPackageStartupMessages(library(GGally))
+suppressPackageStartupMessages(library(corrplot))
+suppressPackageStartupMessages(library(wordcloud))
 
 path <- "../data/euro_football_players.txt"
 
@@ -37,7 +44,8 @@ tb2 <- tb2 %>%
     mutate(tit = as.integer(tit),
            res = str_replace(res, "\\D", "") %>% as.integer()) %>%
     replace_na(list(res = 0)) %>%
-    mutate(games = tit + res,
+    mutate(bmi = kg/(cm/100)^2,
+           games = tit + res,
            position = str_replace(pos,
                                   pattern = "^([A-Z]+)\\(?.*$",
                                   replacement = "\\1"),
@@ -212,6 +220,364 @@ grid.arrange(gg + geom_boxplot(fill = "yellow", alpha = 0.5),
              ncol = 1)
 
 cap <- "Distribuição da altura dos jogadores por função em campo."
+
+#---- position_bar --------------------------------------------------
+
+gg0 <- ggplot(tb2,
+             aes(x = position)) +
+    geom_bar(fill = "tomato", color = "black") +
+    ylab("Número de jogadores") +
+    xlab("Função em campo")
+
+gg1 <- ggplot(tb2,
+              aes(x = red)) +
+    geom_bar(fill = "navyblue", color = "black") +
+    ylab("Número de jogadores") +
+    xlab("Número de cartões vermelhos")
+
+grid.arrange(gg0, gg1, nrow = 1)
+
+cap <-
+    "Distribuição dos jogadores quanto a função em campo e número de cartões vermelhos."
+
+#---- position_pie -----------------------------------------------------
+
+gg0 <- ggplot(tb2,
+              aes(x = 0, fill = position)) +
+    geom_bar(color = "black") +
+    coord_polar(theta = "y") +
+    ylab(NULL) +
+    xlab(NULL) +
+    scale_x_continuous(name = NULL, breaks = NULL) +
+    labs(fill = "Função")
+
+gg1 <- ggplot(tb2,
+              aes(x = 0, fill = factor(red))) +
+    geom_bar(color = "black") +
+    coord_polar(theta = "y") +
+    ylab(NULL) +
+    xlab(NULL) +
+    scale_x_continuous(name = NULL, breaks = NULL) +
+    labs(fill = "Função")
+
+grid.arrange(gg0, gg1, nrow = 1)
+
+cap <- "Distribuição dos jogadores quanto a função em campo e número de cartões vermelhos."
+
+#---- position_donut ---------------------------------------------------
+
+gg0 <- ggplot(tb2,
+              aes(x = 0, fill = position)) +
+    geom_bar(color = "black", width = 1) +
+    coord_polar(theta = "y") +
+    ylab(NULL) +
+    xlab(NULL) +
+    scale_x_continuous(name = NULL, breaks = NULL) +
+    xlim(c(-2, 0.5)) +
+    labs(fill = "Função")
+
+gg1 <- ggplot(tb2,
+              aes(x = 0, fill = position)) +
+    geom_bar(color = "black") +
+    coord_polar(theta = "y") +
+    ylab(NULL) +
+    xlab(NULL) +
+    scale_x_continuous(name = NULL, breaks = NULL) +
+    labs(fill = "Função")
+
+grid.arrange(gg0, gg1, nrow = 1)
+
+cap <- "Distribuição dos jogadores quanto a função em campo."
+
+#---- redcard_waffle ---------------------------------------------------
+
+waffle(table(tb2$red),
+       rows = 30,
+       size = 1,
+       xlab = "Cada unidade é um cartão vermelho")
+
+cap <- "Distribuição dos jogadores quanto ao número de cartões vermelhos."
+
+#---- bmi_qq -----------------------------------------------------------
+
+gg0 <- ggplot(tb2, aes(sample = age)) +
+    geom_qq() +
+    ylab("Idade dos jogadores (anos)") +
+    xlab("Quantis teóricos da distribuição normal padrão")
+
+gg1 <- ggplot(tb2, aes(sample = bmi)) +
+    # facet_wrap(facets = ~position) +
+    geom_qq() +
+    ylab("Índice de massa corporal") +
+    xlab("Quantis teóricos da distribuição normal padrão")
+
+grid.arrange(gg0, gg1, nrow = 1)
+
+cap <- "Distribuição da idade e índice de massa corportal dos jogadores."
+
+#---- yel_stacked_bar --------------------------------------------------
+
+tb2 <- tb2 %>%
+    mutate(yelg = cut(yel,
+                      c(0, 0.5, 1, 2, 4, Inf),
+                      c("0", "1", "2", "3 a 4", "5+"),
+                      include.lowest = TRUE))
+
+gg0 <- ggplot(tb2, aes(x = position, fill = fct_rev(yelg))) +
+    geom_bar(color = "black") +
+    ylab("Número de jogadores") +
+    xlab("Função em campo") +
+    labs(fill = "Cartões\namarelos") +
+    scale_fill_brewer(palette = 7, direction = -1)
+gg0
+
+cap <- "Cartões amarelos por função em campo."
+
+#---- yel_grouped_bar --------------------------------------------------
+
+gg0 <- ggplot(tb2, aes(x = position, fill = yelg)) +
+    geom_bar(color = "black", position = "dodge") +
+    ylab("Número de jogadores") +
+    xlab("Função em campo") +
+    labs(fill = "Cartões\namarelos") +
+    scale_fill_brewer(palette = 7, direction = 1)
+gg0
+
+cap <- "Cartões amarelos por função em campo."
+
+#---- yel_filled_bar ---------------------------------------------------
+
+gg0 <- ggplot(tb2, aes(x = position, fill = fct_rev(yelg))) +
+    geom_bar(color = "black", position = "fill") +
+    ylab("Proporção de jogadores") +
+    xlab("Função em campo") +
+    labs(fill = "Cartões\namarelos") +
+    scale_fill_brewer(palette = 7, direction = -1)
+gg0
+
+cap <- "Cartões amarelos por função em campo."
+
+#---- yel_mosaic -------------------------------------------------------
+
+ggplot(data = tb2) +
+    geom_mosaic(aes(x = product(yelg, position),
+                    fill = yelg,
+                    na.rm = TRUE),
+                alpha = 1,
+                color = "black") +
+    ylab("Número de cartões") +
+    xlab("Função em campo") +
+    labs(fill = "Cartões\namarelos") +
+    scale_fill_brewer(palette = 7, direction = 1)
+
+cap <- "Cartões amarelos por função em campo."
+
+#---- cm_x_kg ----------------------------------------------------------
+
+tb2_gk <- tb2 %>%
+    filter(position == "GK")
+
+gg0 <- ggplot(data = tb2_gk,
+              aes(x = kg, y = cm)) +
+    ylab("Altura do goleiro (cm)") +
+    xlab("Peso do goleiro (kg)")
+
+grid.arrange(gg0 + geom_point(pch = 1, size = 2),
+             gg0 + geom_jitter(pch = 1, size = 2),
+             nrow = 1)
+
+cap <- "Relação altura e peso dos goleiros. O gráfico da direita foi adicionada pertubação para evitar sobreposição dos pontos."
+
+#---- cm_x_kg_x_age ----------------------------------------------------
+
+gg0 <- ggplot(data = tb2_gk,
+              aes(x = kg, y = cm)) +
+    ylab("Altura do goleiro (cm)") +
+    xlab("Peso do goleiro (kg)")
+
+gg1 <- gg0 +
+    geom_point(aes(color = age), size = 2) +
+    scale_color_distiller(palette = "Spectral", direction = -1) +
+    theme_light()
+
+grid.arrange(gg0 + geom_point(aes(size = age), pch = 1),
+             gg1,
+             nrow = 1)
+
+cap <- "Relação altura, peso dos goleiros e idade dos goleiros."
+
+#---- ggpairs ----------------------------------------------------------
+
+ggpairs(tb2_gk[, c("cm", "kg", "age")])
+cap <- "Matriz de diagramas de dispersão."
+
+#---- correlogram ------------------------------------------------------
+
+M <- cor(tb2_gk[, c("cm", "kg", "age", "bmi", "aw")], use = "complete")
+
+par(mfrow = c(1, 2))
+corrplot(M, method = "square", order = "AOE", addCoef.col = "grey")
+corrplot(M, method = "ellipse", order = "AOE", addCoef.col = "grey")
+layout(1)
+
+cap <- "Correlograma das variáveis para os goleiros."
+
+#---- heatmap ----------------------------------------------------------
+
+tb2_gk2 <- tb2_gk %>%
+    top_n(games, n = 20) %>%
+    mutate_at(vars(cm, kg, age, bmi, aw), scale) %>%
+    select(name, cm, kg, age, bmi, aw) %>%
+    drop_na() %>%
+    gather(key = "var", value = "value", -name)
+
+ggplot(tb2_gk2, aes(var, name)) +
+    geom_tile(aes(fill = value), colour = "black") +
+    ylab("Goleiro") +
+    xlab("Variável") +
+    scale_fill_gradient(name = "Valor\npadronizado",
+                        low = "white",
+                        high = "navyblue")
+
+cap <- "Mapa de calor das variáveis para os goleiros."
+
+#---- parallel ---------------------------------------------------------
+
+tb2_gk2 <- tb2_gk2 %>%
+    mutate(var = fct_relevel(var, "bmi", after = Inf))
+
+lb <- tb2_gk2 %>%
+    filter(var == "bmi")
+
+ggplot(tb2_gk2, aes(var, value, color = name)) +
+    geom_point() +
+    geom_text(data = lb,
+              aes(x = var, y = value, label = name),
+              hjust = 0,
+              nudge_x = 0.1,
+              color = "black",
+              check_overlap = FALSE) +
+    geom_line(aes(group = name)) +
+    expand_limits(x = c(1, 7)) +
+    xlab("Variável") +
+    ylab("Valor padronizado") +
+    guides(colour = FALSE)
+
+cap <- "Gráfico de eixos paralelos das variáveis para os goleiros."
+
+#---- spider -----------------------------------------------------------
+
+library(fmsb)
+
+u <- tb2_gk2 %>%
+    spread(key = "var", value = "value")
+u <- bind_rows(u %>% summarise_if(is.numeric, min),
+          u %>% summarise_if(is.numeric, max)) %>%
+    add_column(name = c("min", "max")) %>%
+    bind_rows(u) %>%
+    select(name, everything())
+
+oldpar <- par()
+par(mar = c(0, 0, 0, 0))
+radarchart(u[, -1])
+
+# par(oldpar)
+
+cap <- "Gráfico de radar das variáveis para os goleiros."
+
+#---- cleveland --------------------------------------------------------
+
+set.seed(123)
+s <- sample(unique(tb2$team), size = 15)
+
+u <- tb2 %>%
+    filter(team %in% s) %>%
+    mutate(goleiro = ifelse(position == "GK", "goleiro", "jogador")) %>%
+    group_by(team, goleiro) %>%
+    summarise(cm = mean(cm, na.rm = TRUE))
+
+ggplot(u, aes(cm, team, color = goleiro)) +
+    geom_point() +
+    labs(color = "Função", x = "Altura (cm)", y = "Equipe")
+
+cap <- "Altura média por equipe para goleiros e jogadores da linha."
+
+#---- boxplots ---------------------------------------------------------
+
+gg <- ggplot(tb2, aes(x = position)) +
+    xlab("Função em campo")
+
+gg1 <- gg +
+    geom_boxplot(aes(y = cm), fill = "tomato") +
+    ylab("Altura (cm)")
+
+gg2 <- gg +
+    geom_boxplot(aes(y = kg), fill = "springgreen") +
+    ylab("Peso (kg)")
+
+gg3 <- gg +
+    geom_boxplot(aes(y = age), fill = "pink") +
+    ylab("Idade (anos)")
+
+grid.arrange(gg1, gg2, gg3, nrow = 1)
+
+cap <- "Diagramas de caixa para altura, peso e idade dos jogadores por função."
+
+#---- hexbin -----------------------------------------------------------
+
+gg <- ggplot(tb2, aes(x = cm, y = kg))
+
+grid.arrange(gg + geom_bin2d(),
+             gg + geom_hex(),
+             nrow = 1)
+
+cap <- "Densidade de pontos."
+
+#---- ordered_bar ------------------------------------------------------
+
+tb_gols <- tb2 %>%
+    top_n(goal, n = 20)
+
+ggplot(tb_gols, aes(reorder(name, goal), goal)) +
+    geom_col() +
+    xlab("Jogador") +
+    ylab("Gols marcados") +
+    coord_flip()
+
+cap <- "Jogadores classificados pelo número de gols feito."
+
+#---- lillipop ---------------------------------------------------------
+
+ggplot(tb_gols, aes(reorder(name, goal), goal)) +
+    geom_point() +
+    geom_segment(aes(xend = name, y = 0, yend = goal)) +
+    xlab("Jogador") +
+    ylab("Gols marcados") +
+    coord_flip()
+
+cap <- "Jogadores classificados pelo número de gols feito."
+
+#---- wordcloud --------------------------------------------------------
+
+tb_freq <- tb2 %>%
+    count(country)
+
+par(mar = c(0, 0, 0, 0), mfrow = c(1, 2))
+wordcloud(words = tb_freq[[1]],
+          freq = tb_freq[[2]],
+          random.order = FALSE,
+          col = "navyblue",
+          min.freq = 1,
+          rot.per = 0)
+wordcloud(words = tb2$name,
+          freq = tb2$goal^2,
+          random.order = FALSE,
+          min.freq = 1,
+          col = "green4",
+          max.words = 50,
+          rot.per = 0)
+
+cap <- "Países conforme o número de jogadores de cada nacionalidade e jogadores conforme o número de gols."
 
 #-----------------------------------------------------------------------
 
